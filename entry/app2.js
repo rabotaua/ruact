691d2260b27fa1ddac8eb27e3f62dea40b0d271c
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-09-19 16:47:45 +0300

    fix eslint

--- entry/app2.js
+++ entry/app2.js
@@ -5,20 +5,20 @@ import App2 from '../components/App2'
 
 const app2 = document.getElementById('app2')
 ReactDOM.render(
-  <AppContainer>
-    <App2 />
-  </AppContainer>,
-  app2
+	<AppContainer>
+		<App2 />
+	</AppContainer>,
+	app2
 )
 
 if (module.hot) {
-  module.hot.accept('../components/App2', () => {
-    const NextApp2 = require('../components/App2').default
-    ReactDOM.render(
-      <AppContainer>
-         <NextApp2 />
-      </AppContainer>,
-      app2
-    )
-  })
+	module.hot.accept('../components/App2', () => {
+		const NextApp2 = require('../components/App2').default
+		ReactDOM.render(
+			<AppContainer>
+				<NextApp2 />
+			</AppContainer>,
+			app2
+		)
+	})
 }
